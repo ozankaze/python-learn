404ab923e351bc73a19209c7793ddafa3fa5f640
Zan <ozankaze101@gmail.com>
2022-06-07 19:41:04 +0700

    first commit

---
+++ day.py
@@ -0,0 +1,135 @@
+# print('Hello Zan')
+
+# VARIABLE adalah tempat menyimpan data
+
+# nama = "Hallo ....."
+# print(nama)
+
+# TIPE DATA (DATA TYPE)
+# untuk melihat type daya bisa menggunakan, type(variable)
+
+# angka
+# numberZ = 123
+# print(numberZ)
+
+# string
+# stringZ = "halo kaka"
+# print(stringZ)
+
+# congcatiation
+# pemainBola = "Cristiano"
+# blackPink = "jisoo"
+# print(pemainBola + " Ronaldo")
+# print(pemainBola + " " + blackPink)
+# print("jisoo akan menggung \n di yogyakarta")
+# blackPink.capitalize()
+
+# escaping
+# pubg = "senjata saya pakai"
+# print(pubg + "M4 \" Glacier" )
+
+# aplikasi sederhana menggunakan interaksi input
+# pubg = input("Anda suka pakai senjata apa? ")
+# sniper = input("Anda pakai seniper pake tipe apa? ")
+# print("Halo saya suka bermain PUBG menggunakan senjata " + pubg + " pakai sniper jenis " + sniper)
+
+# boolean ada lah objeak atau tipe data yang nilainya true/false
+# boolean adalah hasil atau nilai sesuatu antara true/false
+# angka1 = 20000
+# angka2 = 100000
+# print(angka1 > angka2)
+# == -> apakah angka1 sama dengan angak2
+# angka 1 nggak "lebih besar" daripada 2
+# if angka1 < angka2: 
+#     print("Ya Betul") #true
+# else: 
+#     print("Ya Salah") #false
+
+# elif
+# uang = input("Uang kamu ada berapa? ")
+# utang = 30000
+
+# if int(uang) < utang:
+#     print("Wah duitnya kurang")
+# elif int(uang) == utang:
+#     print("Kesuwun wis bayar")
+# else:
+#     print("Pak duitmu kelebihan")
+
+# logika operator
+# and(&), or(|), not(!=)
+
+# & jika memakai operator ini syarat1 dan syarat2 harus terpenuhi.
+# | jika memakai operator ini syarat1 atau syarat2, sayaratnya cukup salah satu aah terpenuhi. walaupun ada fals jawabanya tetep benar.
+
+# syarat1 = True
+# syarat2 = False
+
+# if syarat1 | syarat2:
+#     print("ya betul")
+# else:
+#     print("ya sayah")
+
+# putri mencari jodoh
+
+# tamu = "Cewe"
+# baik = True
+# jujur = True
+
+# if baik & jujur:
+#     if tamu == str("Cewe"):
+#         print("Yuk nikah")
+#     else:
+#         print("Kita saudara ajah")
+# else:
+#     print("Kamu bukan tipe ku")
+
+# halooo saya
+# <h1> PUBG </h1>
+# <h2> Valorant </h2>
+# <h2> Dota 2 </h2>
+
+# <h2> Toshiba </h2>
+# <h2> Daihatsu </h2>
+
+# while loop
+
+# count = 0
+
+# while count < 5:
+#     print("Yey akhirnya saya punya M4 Glacier")
+#     count = count+1
+# else:
+#     print("Hup berhenti sampai sini")
+
+# for in loop
+
+# pubgSenjata = ['M4', 'AWM', 'AKM', 'MINI 14']
+
+# for pubg in pubgSenjata:
+#     print("Saya pakai senjata", pubg)
+
+# loop bercabang = 2.50
+
+# angkaPertama = 1
+
+# while angkaPertama < 5:
+#     angkaKedua = 0
+#     while angkaKedua < angkaPertama:
+#         print('*', end="")
+#         angkaKedua = angkaKedua+1
+#     print("End")
+#     angkaPertama = angkaPertama+1
+
+# list -> adalah kumpulan beberapa data dalam variable
+
+# game = ["PUBG", "MOBILE LEGEND", "APEX LEGEND", "VALORANT"]
+# rilisTahun = [2011, 2009, 2007, 2019]
+# buatanNegara = ["TOKYO", 2019, "KOREA", 9090]
+
+# game.append("HUHAK") -> untuk menambahkan
+# game[0] = "LEGEND" -> untuk mengedit
+# del rilisTahun[2] -> untuk menghapus
+
+# print(rilisTahun)
+
