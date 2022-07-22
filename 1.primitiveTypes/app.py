ffe6e8f939880e67ca6b0b516a1c837cda0177a8
Ozan Kaze <ozankaze101@gmail.com>
2022-07-22 16:06:27 +0700

    string

--- 1.primitiveTypes/app.py
+++ 1.primitiveTypes/app.py
@@ -2,3 +2,20 @@
 # rating = 4.88
 # is_published = True
 # course_name = "Halo ka Irine"
+
+# #### String
+# Menyatakan karakter/kalimat bisa berupa huruf angka, dll (diapit tanda " atau ')
+
+# course = "Python Programming"
+# print(len(course))
+# # 18
+# print(course[0])
+# # P
+# print(course[0:3])
+# # Pyt
+# print(course[0:])
+# # Python Programming
+# print(course[:3])
+# # Pyt
+# print(course[:])
+# # Python Programming
