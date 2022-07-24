11c7a7c7dc86d856312ce5c8d3b22f081761def3
Ozan Kaze <ozankaze101@gmail.com>
2022-07-24 19:30:11 +0700

    coba-coba

--- app.py
+++ app.py
@@ -2,3 +2,17 @@
 # y = 5
 # unit_price = 5
 # print('Hello Word')
+
+# QUIZ
+
+# a = 12
+# # b = "bag"
+# # c = "apple"
+# # d = "cat"
+
+# if a == int("10"):
+#     print("a")
+# elif "bag" > "apple" and "bag" > "cat":
+#     print("b")
+# else:
+#     print("c")
